aadeab5cb3d27b09ae4556e46f16cb90ed8f3ba2
AruGayi <36818512+AruGayi@users.noreply.github.com>
2018-03-24 00:44:36 +0530

    Update TestingForTest.java

--- src/com/sample/TestingForTest.java
+++ src/com/sample/TestingForTest.java
@@ -3,6 +3,9 @@ package com.sample;
 public class TestingForTest {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Hello world1");
+		System.out.println("Hello world2");
+		System.out.println("Hello world3");
 	}
 
 }
